ee36d1725dc7d4268cd2420a906d195ebd24a065
Darren Brien <darrenbrien@aol.com>
2020-12-14 08:33:07 +0000

    update publisher to include file name so engines aren't confused between datasets

--- src/publisher.py
+++ src/publisher.py
@@ -1,4 +1,5 @@
 import sys
+from os.path import basename
 import glob
 import boto3
 
@@ -12,10 +13,10 @@ if __name__ == "__main__":
         i for i in res["DeliveryStreamNames"] if i.startswith("streaming_submissions-")
     ]
     batch = []
-    for num, filename in zip(["1", "2", "3", "4"], glob.glob(sys.argv[1])):
+    for filename in glob.glob(sys.argv[1]):
         with open(filename) as csvf:
             for i, row in enumerate(csvf.readlines()):
-                batch.append({"Data": num + row})
+                batch.append({"Data": basename(filename) + " " + row})
                 if len(batch) >= batchsize:
                     client.put_record_batch(
                         DeliveryStreamName=streamname, Records=batch
