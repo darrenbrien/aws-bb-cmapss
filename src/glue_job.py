9b0f8e5119470e3727bd3c781d8f2659911d9d90
Darren Brien <darrenbrien@aol.com>
2020-12-14 13:46:08 +0000

    update glue job and iam roles to all aws-bb-darren*

--- src/glue_job.py
+++ src/glue_job.py
@@ -23,32 +23,33 @@ datasource0 = glueContext.create_dynamic_frame.from_catalog(
 apply_mapping = ApplyMapping.apply(
     frame=datasource0,
     mappings=[
-        ("col0", "long", "unit_number", "long"),
-        ("col1", "long", "cycle", "long"),
-        ("col2", "double", "op_1", "double"),
-        ("col3", "double", "op_2", "double"),
-        ("col4", "double", "op_3", "double"),
-        ("col5", "double", "sensor_measurement_1", "double"),
-        ("col6", "double", "sensor_measurement_2", "double"),
-        ("col7", "double", "sensor_measurement_3", "double"),
-        ("col8", "double", "sensor_measurement_4", "double"),
-        ("col9", "double", "sensor_measurement_5", "double"),
-        ("col10", "double", "sensor_measurement_6", "double"),
-        ("col11", "double", "sensor_measurement_7", "double"),
-        ("col12", "double", "sensor_measurement_8", "double"),
-        ("col13", "double", "sensor_measurement_9", "double"),
-        ("col14", "double", "sensor_measurement_10", "double"),
-        ("col15", "double", "sensor_measurement_11", "double"),
-        ("col16", "double", "sensor_measurement_12", "double"),
-        ("col17", "double", "sensor_measurement_13", "double"),
-        ("col18", "double", "sensor_measurement_14", "double"),
-        ("col19", "double", "sensor_measurement_15", "double"),
-        ("col20", "double", "sensor_measurement_16", "double"),
-        ("col21", "long", "sensor_measurement_17", "long"),
-        ("col22", "long", "sensor_measurement_18", "long"),
-        ("col23", "double", "sensor_measurement_19", "double"),
-        ("col24", "double", "sensor_measurement_20", "double"),
-        ("col25", "double", "sensor_measurement_21", "double"),
+        ("col0", "string", "filename", "string"),
+        ("col1", "long", "unit_number", "long"),
+        ("col2", "long", "cycle", "long"),
+        ("col3", "double", "op_1", "double"),
+        ("col4", "double", "op_2", "double"),
+        ("col5", "double", "op_3", "double"),
+        ("col6", "double", "sensor_measurement_1", "double"),
+        ("col7", "double", "sensor_measurement_2", "double"),
+        ("col8", "double", "sensor_measurement_3", "double"),
+        ("col9", "double", "sensor_measurement_4", "double"),
+        ("col10", "double", "sensor_measurement_5", "double"),
+        ("col11", "double", "sensor_measurement_6", "double"),
+        ("col12", "double", "sensor_measurement_7", "double"),
+        ("col13", "double", "sensor_measurement_8", "double"),
+        ("col14", "double", "sensor_measurement_9", "double"),
+        ("col15", "double", "sensor_measurement_10", "double"),
+        ("col16", "double", "sensor_measurement_11", "double"),
+        ("col17", "double", "sensor_measurement_12", "double"),
+        ("col18", "double", "sensor_measurement_13", "double"),
+        ("col19", "double", "sensor_measurement_14", "double"),
+        ("col20", "double", "sensor_measurement_15", "double"),
+        ("col21", "double", "sensor_measurement_16", "double"),
+        ("col22", "long", "sensor_measurement_17", "long"),
+        ("col23", "long", "sensor_measurement_18", "long"),
+        ("col24", "double", "sensor_measurement_19", "double"),
+        ("col25", "double", "sensor_measurement_20", "double"),
+        ("col26", "double", "sensor_measurement_21", "double"),
         ("partition_0", "string", "year", "string"),
         ("partition_1", "string", "month", "string"),
         ("partition_2", "string", "day", "string"),
@@ -59,12 +60,12 @@ apply_mapping = ApplyMapping.apply(
 
 df = apply_mapping.toDF()
 results_df = (
-    df.groupBy("unit_number")
+    df.groupBy(["filename", "unit_number"])
     .max("cycle")
     .withColumnRenamed("max(cycle)", "failure_cycle")
 )
 
-joined = results_df.join(df, ["unit_number"])
+joined = results_df.join(df, ["filename", "unit_number"])
 
 rul_df = joined.withColumn("failure_cycle", joined["failure_cycle"] - joined["cycle"])
 
@@ -74,7 +75,7 @@ finished = glueContext.write_dynamic_frame.from_options(
     frame=final,
     connection_type="s3",
     connection_options={
-        "path": "s3://datalake-curated-datasets-907317471167-us-east-1-qkg9331",
+        "path": "s3://datalake-curated-datasets-907317471167-us-east-1-gismq40",
         "partitionKeys": ["year", "month", "day", "hour"],
     },
     format="glueparquet",
